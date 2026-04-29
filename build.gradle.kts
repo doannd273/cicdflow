9c4ab964ebfffd6c3e53017f159751b3f38ac450
doannd3 <doannd3@ghtk.co>
2026-04-29 15:33:44 +0700

    remove code test

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,23 @@ plugins {
     alias(libs.plugins.firebase.appdistribution) apply false
     alias(libs.plugins.detekt) apply false
 }
+
+subprojects {
+    plugins.withId("io.gitlab.arturbosch.detekt") {
+
+        extensions.configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+            toolVersion = libs.versions.detekt.get()
+
+            buildUponDefaultConfig = true
+            allRules = false
+
+            config.setFrom("$rootDir/config/detekt/detekt.yml")
+            baseline = file("$rootDir/config/detekt/baseline.xml")
+
+            source.setFrom(
+                "src/main/java",
+                "src/main/kotlin"
+            )
+        }
+    }
+}
